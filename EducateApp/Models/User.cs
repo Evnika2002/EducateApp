fdbe5f68353f24edc819701b6d808686e84f753b
Евника Алексеевна <Matafonovaevnukapvkc2-1@mail.ru>
2021-10-26 16:21:32 +0500

    Сохдание контекстных данных, написание строки подключения к любому экземпляру MS SQL Server, связи контекста и строки в файле Startap

---
+++ EducateApp/Models/User.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+
+namespace EducateApp.Models
+{
+    public class User : IdentityUser
+    {
+        //Дополнительные поля для каждого пользователя
+        //для преподователя могут понадобиться данные о ФИО
+
+        //Сообщение об ошибке при валидации на стороне клиента
+        [Required(ErrorMessage = "Введите фамилию")]
+
+        //отображение Фамилии вместо LastName
+        [Display(Name = "Фамилия")]
+        public string LastName { get; set; }
+
+        [Required(ErrorMessage = "Введите имя")]
+        [Display(Name = "Имя")]
+        public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "Введите отчество")]
+        [Display(Name = "Отчество")]
+        public string Patronymic { get; set; }
+
+        //навигационные свойства
+    }
+}
