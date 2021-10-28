ce3ac840a8471cc6b056f093a955afc40f029eee
Евника Алексеевна <Matafonovaevnukapvkc2-1@mail.ru>
2021-10-28 09:42:32 +0500

    Добавление регистрации пользователя

---
+++ EducateApp/Controllers/AccountController.cs
@@ -0,0 +1,58 @@
+﻿using EducateApp.Models;
+using EducateApp.ViewModels;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace EducateApp.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly UserManager<User> _userManager;
+        private readonly SignInManager<User> _signInManager;
+
+        public AccountController(UserManager<User> userManager, SignInManager<User> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+        [HttpPost]
+
+        //Метод принимает описаную нами модель для представления (значения введенные в элементы управления на странице, метод post)
+        public async Task<IActionResult> Register(RegisterViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                //Создание экземпляра Users класса Users  и установка его свойствам значениям из модели
+                User user = new User { 
+                    LastName=model.LastName,
+                    FirstName=model.FirstName,
+                    Patronymic=model.Patronymic,
+                    Email = model.Email,
+                    UserName = model.Email 
+                };
+                // добавляем пользователя
+                var result = await _userManager.CreateAsync(user, model.Password);
+                if (result.Succeeded)
+                {
+                    // установка куки
+                    await _signInManager.SignInAsync(user, false);
+                    return RedirectToAction("Index", "Home");
+                }
+                else
+                {
+                    foreach (var error in result.Errors)
+                    {
+                        ModelState.AddModelError(string.Empty, error.Description);
+                    }
+                }
+            }
+            return View(model);  //возвращение модели в представление
+        }
+    }
+}
---
+++ EducateApp/ViewModels/Account/RegisterViewModel.cs
@@ -0,0 +1,37 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EducateApp.ViewModels
+{
+    //Т.е какие свойства (поля) "модель" нужно заполнить на определенной странице Html "представление"
+    // на странице с регистрацией помимо E-mail  пароля нужно ввести информацию о преподователе - его ФИО
+    public class RegisterViewModel
+    {
+        [Required(ErrorMessage = "Введите E - mail")]
+        [Display(Name = "E - mail")]
+        [DataType(DataType.EmailAddress)]   // тип элемента управления на станице 
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Введите фамилию")]
+        [Display(Name = "Фамилия")]
+        public string LastName { get; set; }
+
+        [Required(ErrorMessage = "Введите имя")]
+        [Display(Name = "Имя")]
+        public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "Введите отчество")]
+        [Display(Name = "Отчество")]
+        public string Patronymic { get; set; }
+
+        [Required(ErrorMessage = "Введите пароль")]
+        [Display(Name = "Пароль")]
+        [DataType(DataType.Password)]          // тип элемента управления на станице
+        public string Password { get; set; }
+
+        [Required(ErrorMessage = "Повторите ввод пароля")]
+        [Compare("Password", ErrorMessage = "Пароли не совпадают")]     //механизм, который сверяет текущее значение PasswordConfirm, Password
+        [DataType(DataType.Password)]          // тип элемента управления на станице
+        [Display(Name = "Подтвердить пароль")]
+        public string PasswordConfirm { get; set; }
+    }
+}
